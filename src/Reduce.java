be51a453e08a6bdd2f89a977f055b43025fd52f8
Oscar Kolodziejczyk <oscar.kolodziejczyk@mail.utoronto.ca>
2023-09-12 23:15:50 -0400

    Correct Reduce file for Assignment 1

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,20 +1,19 @@
 public class Reduce {
     public static void main(String[] args) {
-
+        int amt = reduce(10);
+        System.out.println(amt);
     }
 
     public static int reduce(int x) {
         int steps = 0;
-        if (x == 0){
-            return steps
+        while (x > 0) {
+            if (x % 2 == 0) {
+                x = x / 2;
+            } else {
+                x = x - 1;
+            }
+            steps++;
         }
-        else if (x % 2 == 0) {
-            x = x / 2;
-        }
-        else {
-            x - 1;
-        }
-        steps++;
-        x = reduce(x);
+        return steps;
     }
 }
